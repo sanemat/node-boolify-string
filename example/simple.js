bd6c247e519cea21f462b746ebbb0d38255e0b9f
sanemat <o.gata.ken@gmail.com>
2014-06-29 23:11:21 +0900

    Add example

--- example/simple.js
+++ example/simple.js
@@ -8,6 +8,18 @@
 
 'use strict';
 
-var boolifyString = require('../');
+var boolifyString = require('../boolify-string.js');
 
 boolifyString.awesome(); // "awesome"
+//value type result
+//{} オブジェクト true
+//"hoge" 文字列 true
+//"" 文字 false
+//1 数値 true
+//-1 数値 true
+//0 数値 false
+//  [] 配列 true
+//true 真偽値 true
+//false 真偽値 false
+//undefined undefined false
+//null null false

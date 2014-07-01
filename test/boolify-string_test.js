88f57b8ca2af1eede999e41b5d9070b408974a91
sanemat <o.gata.ken@gmail.com>
2014-07-01 15:43:06 +0900

    Separate truthy and falsy

--- test/boolify-string_test.js
+++ test/boolify-string_test.js
@@ -4,46 +4,51 @@ var boolifyString = require('../lib/boolify-string.js');
 var assert = require('power-assert');
 
 describe('boolifyString', function () {
+  describe('truthy strings', function(){
+    it('true should be true', function () {
+      assert.equal(boolifyString('true'), true);
+    });
+    it('TRUE should be true', function () {
+      assert.equal(boolifyString('TRUE'), true);
+    });
+    it('True should be true', function () {
+      assert.equal(boolifyString('True'), true);
+    });
+    it('{} should be true', function () {
+      assert.equal(boolifyString('{}'), true);
+    });
+    it('some string should be true', function () {
+      assert.equal(boolifyString('some string'), true);
+    });
+    it('1 should be true', function () {
+      assert.equal(boolifyString('1'), true);
+    });
+    it('-1 should be true', function () {
+      assert.equal(boolifyString('-1'), true);
+    });
+    it('[] should be true', function () {
+      assert.equal(boolifyString('[]'), true);
+    });
+  });
 
-  it('should be true', function () {
-    assert.equal(boolifyString('true'), true);
+  describe('falsy string', function(){
+    it('should be false', function () {
+      assert.equal(boolifyString('false'), false);
+    });
+    it('should be false', function () {
+      assert.equal(boolifyString(''), false);
+    });
+    it('should be false', function () {
+      assert.equal(boolifyString('0'), false);
+    });
+    it('should be false', function () {
+      assert.equal(boolifyString('undefined'), false);
+    });
+    it('should be false', function () {
+      assert.equal(boolifyString('null'), false);
+    });
   });
-  it('should be true', function () {
-    assert.equal(boolifyString('TRUE'), true);
-  });
-  it('should be true', function () {
-    assert.equal(boolifyString('True'), true);
-  });
-  it('should be false', function () {
-    assert.equal(boolifyString('false'), false);
-  });
-  it('should be true', function () {
-    assert.equal(boolifyString('{}'), true);
-  });
-  it('should be true', function () {
-    assert.equal(boolifyString('foo'), true);
-  });
-  it('should be false', function () {
-    assert.equal(boolifyString(''), false);
-  });
-  it('should be true', function () {
-    assert.equal(boolifyString('1'), true);
-  });
-  it('should be true', function () {
-    assert.equal(boolifyString('-1'), true);
-  });
-  it('should be false', function () {
-    assert.equal(boolifyString('0'), false);
-  });
-  it('should be true', function () {
-    assert.equal(boolifyString('[]'), true);
-  });
-  it('should be false', function () {
-    assert.equal(boolifyString('undefined'), false);
-  });
-  it('should be false', function () {
-    assert.equal(boolifyString('null'), false);
-  });
+
   it('should be true', function(){
     assert.equal(boolifyString(true), true);
   });

5f1968632bc32c4dadd098cdbc5683a278460bdf
sanemat <o.gata.ken@gmail.com>
2014-07-01 14:55:11 +0900

    Add true/false/0/undefined/null/(empty) implimentation

--- test/boolify-string_test.js
+++ test/boolify-string_test.js
@@ -17,6 +17,33 @@ describe('boolifyString', function () {
   it('should be false', function () {
     assert.equal(boolifyString('false'), false);
   });
+  it('should be true', function () {
+    assert.equal(boolifyString('{}'), true);
+  });
+  it('should be true', function () {
+    assert.equal(boolifyString('foo'), true);
+  });
+  it('should be false', function () {
+    assert.equal(boolifyString(''), false);
+  });
+  it('should be true', function () {
+    assert.equal(boolifyString('1'), true);
+  });
+  it('should be true', function () {
+    assert.equal(boolifyString('-1'), true);
+  });
+  it('should be false', function () {
+    assert.equal(boolifyString('0'), false);
+  });
+  it('should be true', function () {
+    assert.equal(boolifyString('[]'), true);
+  });
+  it('should be false', function () {
+    assert.equal(boolifyString('undefined'), false);
+  });
+  it('should be false', function () {
+    assert.equal(boolifyString('null'), false);
+  });
   it('should be true', function(){
     assert.equal(boolifyString(true), true);
   });
